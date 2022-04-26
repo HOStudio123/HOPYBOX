79192edc62e7dd1a733dd7e52a3ec140e428e252
HOStudio123 <98937801+HOStudio123@users.noreply.github.com>
2022-04-26 21:28:45 +0800

    Delete ho_error.py

--- ho_error.py
+++
@@ -1,6 +0,0 @@
-from ho_logs import *
-def error_a(error,pattern,text,value):
-  print("\033[0m\033[31;1m× {} in {} '{}':\n╰─ {}  ".format(error,pattern,value,text))
-  wlog(value,"× {} in {} '{}':\n╰─ {}  ".format(error,pattern,value,text))
-def error_b(text,anwer):
-  print('\033[32;1m{}:Your response (\'{}\') was not one of the expected responses: y, n Proceed (Y/n)?'.format(text,anwer))
