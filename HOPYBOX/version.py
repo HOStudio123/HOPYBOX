0f318ae303f77aff655baa3ecf3783e2d3f341c7
HOStudio123 <98937801+HOStudio123@users.noreply.github.com>
2022-05-02 09:44:23 +0800

    Add files via upload

---
+++ HOPYBOX/version.py
@@ -0,0 +1,3 @@
+from platform import system,python_version
+version = 'HOPYBOX 1.4.0 (default, May 1 2022, 9:25:11)\n[Python {}] on {}\nType "help" for more information'.format(python_version(),system())
+version_number = 14000
