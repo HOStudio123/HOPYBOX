a0e651506d595ac91d4b3bfb56593784e58183f0
HOStudio <hostudio.hopybox@foxmail.com>
2024-06-13 13:09:58 +0800

    Add files via upload

--- HOPYBOX/__init__.py
+++ HOPYBOX/__init__.py
@@ -1,5 +1,6 @@
 '''
-Copyright (c) 2022-2023 HOStudio123(ChenJinlin).
+
+Copyright (c) 2022-2024 HOStudio123(ChenJinlin).
 All Rights Reserved.
 
 Please view help by visiting the website at https://hopybox.github.io.
