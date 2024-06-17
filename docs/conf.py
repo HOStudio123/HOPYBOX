3d5cd6e3c13e82027372743dbf17686e574e73c1
HOStudio <hostudio.hopybox@foxmail.com>
2024-06-17 08:57:46 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'default'
 html_static_path = ['_static']
 html_theme_options = {
 }
