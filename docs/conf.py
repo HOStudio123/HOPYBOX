733c40ba9ba1d40054701ee095ce2aad8ebf1a52
HOStudio123 <hostudio.hopybox@foxmail.com>
2024-07-16 12:17:47 +0800

    update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,36 +1,42 @@
-# -*- coding: utf-8 -*-
+# conf.py
 
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
+
+# Project information
 project = 'HOPYBOX'
-copyright = '2022~2024'
-author = 'HOStudio123'
-release = '1.9.6'
+author = 'HOStudio Development Team'
+release = '0.1.0'
+
+# General configuration
 extensions = [
-    'sphinx.ext.autodoc', 
-    'sphinx.ext.napoleon',  
-    'sphinx.ext.viewcode', 
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx_rtd_theme',
 ]
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
 
-html_theme = 'custom'
+# Options for HTML output
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-html_theme_options = {
-}
 
-latex_elements = {
-    'papersize': 'a4paper',
-    'pointsize': '10pt',
-}
-
-
-intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
-
-napoleon_google_docstring = True
-napoleon_numpy_docstring = False
-
+# Custom CSS
 def setup(app):
     app.add_css_file('custom.css')
+
+# Napoleon settings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
