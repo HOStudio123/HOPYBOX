880bdb1871b51f6bb20baaab769da6b407403b42
HOStudio123 <hostudio.hopybox@foxmail.com>
2024-07-16 12:09:56 +0800

    update

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@ sys.path.insert(0, os.path.abspath('.'))
 project = 'HOPYBOX'
 copyright = '2022~2024'
 author = 'HOStudio123'
-release = '1.8.0'
+release = '1.9.6'
 extensions = [
     'sphinx.ext.autodoc', 
     'sphinx.ext.napoleon',  
@@ -32,5 +32,7 @@ intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
 napoleon_google_docstring = True
 napoleon_numpy_docstring = False
 
+html_static_path = ['_static']
+
 def setup(app):
-    pass
+    app.add_css_file('custom.css')
