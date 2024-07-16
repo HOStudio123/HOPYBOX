48fc5a88a4e16a85b5f5513cdd55e0b808af0f32
HOStudio123 <hostudio.hopybox@foxmail.com>
2024-07-16 15:58:27 +0800

    update

--- docs/conf.py
+++ docs/conf.py
@@ -6,8 +6,8 @@ sys.path.insert(0, os.path.abspath('.'))
 
 # Project information
 project = 'HOPYBOX'
-author = 'HOStudio Development Team'
-release = '0.1.0'
+author = 'HOStudio123'
+release = '1.9.6'
 
 # General configuration
 extensions = [
@@ -27,16 +27,3 @@ html_static_path = ['_static']
 # Custom CSS
 def setup(app):
     app.add_css_file('custom.css')
-
-# Napoleon settings
-napoleon_google_docstring = True
-napoleon_numpy_docstring = True
-napoleon_include_init_with_doc = False
-napoleon_include_private_with_doc = False
-napoleon_include_special_with_doc = True
-napoleon_use_admonition_for_examples = False
-napoleon_use_admonition_for_notes = False
-napoleon_use_admonition_for_references = False
-napoleon_use_ivar = False
-napoleon_use_param = True
-napoleon_use_rtype = True
