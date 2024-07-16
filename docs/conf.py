d2658d5c2f05e2fac4890558c889cd4facf055c7
HOStudio123 <hostudio.hopybox@foxmail.com>
2024-07-16 20:39:56 +0800

    update

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 # Options for HTML output
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'custom'
 html_static_path = ['_static']
 
 # Custom CSS
