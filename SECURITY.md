632a5f4607b15c978d9d40d5aa4ec3e8d9d9680d
HOStudio123 <hostudio.hopybox@foxmail.com>
2025-01-26 13:58:46 +0800

    upload

---
+++ SECURITY.md
@@ -0,0 +1,11 @@
+# Security Policy
+
+## Supported Versions
+
+| Version | Supported          |
+| ------- | ------------------ |
+| 2.0.0   | :white_check_mark: |
+
+## Reporting a Vulnerability
+
+No code vulnerabilities
--- hopybox/__init__.py
+++ hopybox/__init__.py
@@ -1,6 +1,9 @@
-"""
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Copyright (c) 2022-2025 HOStudio123 (hostudio.hopybox@foxmail.com)
 
-Copyright (c) 2022-2024 HOStudio123.
-All Rights Reserved.
-
-"""
+__title__ = 'HOPYBOX'
+__version__ = '3.0.0'
+__version_number__ = int(__version__.replace('.',''))
+__author__ = 'HOStudio123'
+__license__ = 'GPL-3.0'
